bb66df8ebe9baf687ecbd76fc68a123b3bf425e3
Dave Shifflett <dave@corpulentcoffee.com>
2013-11-16 23:39:46 -0600

    New caching Column for general Table
    This adjusts the current "general" table creation code to include a new
    "caching" header and also provides an upgrade path for an installation
    that already has a "general" table to have the "caching" column added.

--- awesometts/config.py
+++ awesometts/config.py
@@ -17,8 +17,35 @@ tblexit = cursor.fetchall()
 
 
 if len (tblexit) < 1:
-	cursor.execute("CREATE TABLE general (automaticQuestions numeric, automaticAnswers numeric, file_howto_name numeric, file_max_length numeric, file_extension text, subprocessing numeric, TTS_KEY_Q numeric, TTS_KEY_A numeric)")
-	cursor.execute("INSERT INTO general VALUES (0, 0, 1, 100, 'mp3', 1, ?, ?)", (Qt.Key_F3, Qt.Key_F4))
+	cursor.execute(
+		"CREATE TABLE general ("
+			"automaticQuestions numeric,"
+			"automaticAnswers numeric,"
+			"file_howto_name numeric,"
+			"file_max_length numeric,"
+			"file_extension text,"
+			"subprocessing numeric,"
+			"TTS_KEY_Q numeric,"
+			"TTS_KEY_A numeric,"
+			"caching numeric"
+		")"
+	)
+	cursor.execute(
+		"INSERT INTO general "
+		"VALUES (0, 0, 1, 100, 'mp3', 1, ?, ?, 1)",
+		(Qt.Key_F3, Qt.Key_F4)
+	)
+
+else:
+	cursor.execute("PRAGMA table_info(general)")
+	for r in cursor:
+		if r['name'] == 'caching':
+			break
+	else:
+		cursor.execute(
+			"ALTER TABLE general "
+			"ADD COLUMN caching numeric DEFAULT 1"
+		)
 
 cursor.execute("SELECT * FROM general")
 

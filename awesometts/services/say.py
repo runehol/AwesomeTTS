ef3dcd9fc819697785a9cf2159fe837432ec9ce3
Dave Shifflett <dave@corpulentcoffee.com>
2014-03-01 17:08:38 -0600

    Linting of awesometts.services.say module
    Brings the `say` command implementation module mostly into compliance
    with the coding standards of the pylint checker.

--- awesometts/services/say.py
+++ awesometts/services/say.py
@@ -2,8 +2,9 @@
 
 # AwesomeTTS text-to-speech add-on for Anki
 #
-# Copyright (C) 2010-2012  Anki AwesomeTTS Development Team
+# Copyright (C) 2010-2014  Anki AwesomeTTS Development Team
 # Copyright (C) 2010-2012  Arthur Helfstein Fragoso
+# Copyright (C) 2014       Dave Shifflett
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -19,83 +20,119 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 
-from PyQt4 import QtGui,QtCore
-import re, subprocess
-from anki.utils import stripHTML, isMac
-from urllib import quote_plus
-import awesometts.config as config
-import awesometts.util as util
-from subprocess import Popen, PIPE, STDOUT
+"""
+Service implementation for OS X's say command
+"""
+
+__all__ = ['TTS_service']
+
+import re
+from subprocess import check_output, Popen, PIPE, STDOUT
+from PyQt4 import QtGui
+from anki.utils import isMac, stripHTML
+from awesometts.util import generateFileName
+
 
 if isMac:
+    SERVICE = 'say'
 
-	vlist = subprocess.check_output("say -v ? |sed 's/  .*//'", shell=True)
+    VOICES = check_output("say -v ? |sed 's/  .*//'", shell=True)
+    VOICES = VOICES.split('\n')
+    VOICES.pop()
 
-	voicelist = vlist.split('\n')
-	voicelist.pop()
+    def play(text, voice):
+        text = re.sub(
+            r'\[sound:.*?\]',
+            '',
+            stripHTML(text.replace('\n', '')).encode('utf-8'),
+        )
 
-	def playOSXsayTTS(text, voice):
-		text = re.sub("\[sound:.*?\]", "", stripHTML(text.replace("\n", "")).encode('utf-8'))
-		subprocess.Popen(['say', '-v', voice, text], stdin=PIPE, stdout=PIPE, stderr=STDOUT).communicate()
+        Popen(
+            ['say', '-v', voice, text],
+            stdin=PIPE,
+            stdout=PIPE,
+            stderr=STDOUT,
+        ).communicate()
 
+    def play_html(fromtag):
+        for item in fromtag:
+            text = ''.join(item.findAll(text=True))
+            voice = item['voice']
+            play(text, voice)
 
-	def playfromtagOSXsayTTS(fromtag):
-		print fromtag
-		for item in fromtag:
-			match = re.match("(.*?):(.*)", item, re.M|re.I)
-			playOSXsayTTS(match.group(2), match.group(1))
+    def play_tag(fromtag):
+        for item in fromtag:
+            match = re.match(r'(.*?):(.*)', item, re.M|re.I)
+            play(match.group(2), match.group(1))
 
-	def playfromHTMLtagOSXsayTTS(fromtag):
-		for item in fromtag:
-			text = text = ''.join(item.findAll(text=True))
-			voice = item['voice']
-			playOSXsayTTS(text, voice)
+    def record(form, text):
+        fg_layout.default_voice = form.comboBoxSay.currentIndex()
 
-	def recordOSXsayTTS(text, voice):
-		text = re.sub("\[sound:.*?\]", "", stripHTML(text.replace("\n", "")).encode('utf-8'))
-		filename_aiff = util.generateFileName(text, 'say', 'iso-8859-1', '.aiff')
-		filename_mp3 = util.generateFileName(text, 'say', 'iso-8859-1', '.mp3')
-		subprocess.Popen(['say', '-v', voice, '-o', filename_aiff, text], stdin=PIPE, stdout=PIPE, stderr=STDOUT).wait()
-		subprocess.Popen(['lame', '--quiet', filename_aiff, filename_mp3], stdin=PIPE, stdout=PIPE, stderr=STDOUT).wait()
-		subprocess.Popen(['rm', filename_aiff], stdin=PIPE, stdout=PIPE, stderr=STDOUT).wait()
-		return filename_mp3.decode('utf-8')
+        text = re.sub(
+            r'\[sound:.*?\]',
+            '',
+            stripHTML(text.replace('\n', '')).encode('utf-8'),
+        )
+        voice = VOICES[form.comboBoxSay.currentIndex()]
 
-	def filegenerator_layout(form):
-		global DefaultSayVoice
-		verticalLayout = QtGui.QVBoxLayout()
-		textEditlabel = QtGui.QLabel()
-		textEditlabel.setText("Voice:")
-		form.comboBoxSay = QtGui.QComboBox()
-		form.comboBoxSay.addItems(voicelist)
-		form.comboBoxSay.setCurrentIndex(DefaultSayVoice) # get Default
+        filename_aiff = generateFileName(text, SERVICE, 'iso-8859-1', '.aiff')
+        filename_mp3 = generateFileName(text, SERVICE, 'iso-8859-1', '.mp3')
 
-		verticalLayout.addWidget(textEditlabel)
-		verticalLayout.addWidget(form.comboBoxSay)
-		return verticalLayout
+        Popen(
+            ['say', '-v', voice, '-o', filename_aiff, text],
+            stdin=PIPE,
+            stdout=PIPE,
+            stderr=STDOUT,
+        ).wait()
 
-	def recordOSXsayTTS_form(form, text):
-		global DefaultSayVoice
-		DefaultSayVoice = form.comboBoxSay.currentIndex() #set new Default
-		return recordOSXsayTTS(text, voicelist[form.comboBoxSay.currentIndex()])
+        Popen(
+            ['lame', '--quiet', filename_aiff, filename_mp3],
+            stdin=PIPE,
+            stdout=PIPE,
+            stderr=STDOUT,
+        ).wait()
 
-	def filegenerator_run(form):
-		global DefaultSayVoice
-		DefaultSayVoice = form.comboBoxSay.currentIndex() #set new Default
-		return recordOSXsayTTS(unicode(form.texttoTTS.toPlainText()), voicelist[form.comboBoxSay.currentIndex()])
+        Popen(
+            ['rm', filename_aiff],
+            stdin=PIPE,
+            stdout=PIPE,
+            stderr=STDOUT,
+        ).wait()
 
-	def filegenerator_preview(form):
-		return playOSXsayTTS(unicode(form.texttoTTS.toPlainText()), voicelist[form.comboBoxSay.currentIndex()])
-	
-	DefaultSayVoice = 0
+        return filename_mp3.decode('utf-8')
 
-	TTS_service = {'say' : {
-	'name': 'OSX Say',
-	'play' : playOSXsayTTS,
-	'playfromtag' : playfromtagOSXsayTTS,
-	'playfromHTMLtag' : playfromHTMLtagOSXsayTTS,
-	'record' : recordOSXsayTTS_form,
-	'filegenerator_layout': filegenerator_layout,
-	'filegenerator_preview': filegenerator_preview,
-	'filegenerator_run': filegenerator_run}}
+    def fg_layout(form):
+        form.comboBoxSay = QtGui.QComboBox()
+        form.comboBoxSay.addItems(VOICES)
+        form.comboBoxSay.setCurrentIndex(fg_layout.default_voice)
 
+        text_label = QtGui.QLabel()
+        text_label.setText("Voice:")
 
+        vertical_layout = QtGui.QVBoxLayout()
+        vertical_layout.addWidget(text_label)
+        vertical_layout.addWidget(form.comboBoxSay)
+
+        return vertical_layout
+
+    def fg_preview(form):
+        return play(
+            unicode(form.texttoTTS.toPlainText()),
+            VOICES[form.comboBoxSay.currentIndex()]
+        )
+
+    def fg_run(form):
+        return record(form, unicode(form.texttoTTS.toPlainText()))
+
+    fg_layout.default_voice = 0
+
+    TTS_service = {SERVICE: {
+        'name': "OS X Say",
+        'play': play,
+        'playfromHTMLtag': play_html,
+        'playfromtag': play_tag,
+        'record': record,
+        'filegenerator_layout': fg_layout,
+        'filegenerator_preview': fg_preview,
+        'filegenerator_run': fg_run,
+    }}

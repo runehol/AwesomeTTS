521c8e697dba731f9e9b55c795563a336a0ca024
Dave Shifflett <dave@corpulentcoffee.com>
2014-03-15 11:58:04 -0500

    Clean up config interface
    Shortens some of the internal instance variables as well as eliminating
    some of the complications with interacting with get() and put().

--- awesometts/config.py
+++ awesometts/config.py
@@ -56,15 +56,15 @@ COLUMN_ALIASES = [
 ]
 
 
-def get(dummy, *dummy_addl):
+def get(dummy):
     """
     Replaced by get method from the Config class instance.
     """
 
-    return {}
+    return
 
 
-def put(dummy):
+def put(**dummy):
     """
     Replaced by put method from the Config class instance.
     """
@@ -74,11 +74,11 @@ def put(dummy):
 
 class Config(object):
     __slots__ = [
-        '_db',                  # path to sqlite3 database
-        '_table',               # table where preferences are stored
-        '_column_definitions',  # map of get() names to column definitions
-        '_column_aliases',      # map of aliased names to their actual names
-        '_cache',               # in-memory lookup of preferences
+        '_db',           # path to sqlite3 database
+        '_table',        # sqlite3 table where preferences are stored
+        '_definitions',  # map of official lookup names to column definitions
+        '_aliases',      # map of alias lookup names to official lookup names
+        '_cache',        # in-memory lookup of preferences
     ]
 
     _RE_NONALPHANUMERIC = re(r'[^a-z0-9]')
@@ -88,22 +88,22 @@ class Config(object):
 
         return cls._RE_NONALPHANUMERIC.sub('', name.lower())
 
-    def __init__(self, db, table, column_definitions, column_aliases):
+    def __init__(self, db, table, definitions, aliases):
 
         self._db = db
 
         self._table = table
 
-        self._column_definitions = {
+        self._definitions = {
             self._normalize(definition[0]): definition
             for definition
-            in column_definitions
+            in definitions
         }
 
-        self._column_aliases = {
-            self._normalize(friendly_name): self._normalize(database_name)
-            for friendly_name, database_name
-            in column_aliases
+        self._aliases = {
+            self._normalize(alias_name): self._normalize(official_name)
+            for alias_name, official_name
+            in aliases
         }
 
         self._cache = {}
@@ -129,7 +129,7 @@ class Config(object):
             missing_definitions = [
                 definition
                 for definition
-                in self._column_definitions.values()
+                in self._definitions.values()
                 if definition[0].lower() not in existing_columns
             ]
 
@@ -158,7 +158,7 @@ class Config(object):
                 )
 
         else:
-            all_definitions = self._column_definitions.values()
+            all_definitions = self._definitions.values()
 
             cursor.execute('CREATE TABLE %s (%s)' % (
                 self._table,
@@ -182,59 +182,48 @@ class Config(object):
             )
 
         row = cursor.execute('SELECT * FROM %s' % self._table).fetchone()
-        for name, definition in self._column_definitions.items():
+        for name, definition in self._definitions.items():
             self._cache[name] = definition[3](row[definition[0]])
 
         cursor.close()
         connection.close()
 
-    def get(self, name, *addl_names):
+    def get(self, name):
 
-        def single(name):
-            name = self._normalize(name)
-            if name in self._column_aliases:
-                name = self._column_aliases[name]
+        name = self._normalize(name)
+        return self._cache[
+            self._aliases[name] if name in self._aliases
+            else name
+        ]
 
-            return self._cache[name]
-
-        return (
-            single(name) if not addl_names
-            else {name: single(name) for name in [name] + list(addl_names)}
-        )
-
-    # FIXME temporary
-    def __getattr__(self, name):
-
-        return self.get(name)
-
-    def put(self, column_updates):
+    def put(self, **updates):
 
         # remap dict into a list of (name, definition, new value)-tuples
-        column_updates = [
+        updates = [
             (
                 name,
-                self._column_definitions[name],
+                self._definitions[name],
                 value,
             )
             for name, value
             in [
                 (
-                    name_or_alias if name_or_alias not in self._column_aliases
-                    else self._column_aliases[name_or_alias],
+                    name_or_alias if name_or_alias not in self._aliases
+                    else self._aliases[name_or_alias],
                     value,
                 )
                 for name_or_alias, value
                 in [
                     (self._normalize(unnormalized_name_or_alias), value)
                     for unnormalized_name_or_alias, value
-                    in column_updates.items()
+                    in updates.items()
                 ]
             ]
             if value != self._cache[name]  # filter out unchanged values
         ]
 
         # update in-memory store of the values
-        for name, definition, value in column_updates:
+        for name, definition, value in updates:
             self._cache[name] = value
 
         # open database connection
@@ -249,13 +238,13 @@ class Config(object):
                 ', '.join([
                     "%s=?" % definition[0]
                     for name, definition, value
-                    in column_updates
+                    in updates
                 ]),
             ),
             tuple(
                 definition[4](value)
                 for name, definition, value
-                in column_updates
+                in updates
             ),
         )
 

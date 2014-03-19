ac1f72ec57a61f2ef39a41a821b6bf8c3d58e828
Dave Shifflett <dave@corpulentcoffee.com>
2014-03-18 22:43:21 -0500

    Implement buffering in BufferedLogger
    This is currently setup using on/off for the handlers, but debug levels
    probably need to be configured instead.

--- awesometts/classes/logger.py
+++ awesometts/classes/logger.py
@@ -25,11 +25,12 @@ Logger support across entire add-on
 __all__ = ['Logger']
 
 import logging
+from sys import stdout
 
 
 def Logger(  # factory masquerading as a constructor, pylint: disable=C0103
     name,
-    stdout_flag, file_flag, file_path,
+    stdout_flag, file_flag, file_path, file_encoding,
 ):
     """
     Returns a logger with the given name after configuring it. If called
@@ -49,11 +50,12 @@ def Logger(  # factory masquerading as a constructor, pylint: disable=C0103
         stdout_flag,
         file_flag,
         file_path,
+        file_encoding,
     )
     return new_instance
 
 
-class BufferedLogger(logging.Logger):  # many inherited, pylint: disable=R0904
+class BufferedLogger(logging.Logger):  # lots inherited, pylint: disable=R0904
     """
     Extends the built-in logger to support buffered logs, i.e. logs that
     can be written to before handlers are setup. This is handy for when
@@ -63,17 +65,18 @@ class BufferedLogger(logging.Logger):  # many inherited, pylint: disable=R0904
 
     __slots__ = [
         '_activated',    # True if we are activated, False otherwise
-        '_buffer',       # log messages that come in before activation
+        '_buffer',       # list of log messages that come in before activation
         '_stdout_flag',  # key to look for in activate() call for stdout
+        '_stdout_sh',    # StreamHandler to stdout; always initialized
         '_file_flag',    # key to look for in activate() call for file output
-        '_file_path',    # path to a log file for use if activated
+        '_file_fh',      # FileHandler for log file; delayed initialization
     ]
 
     def __init__(self, *args, **kwargs):
         """
         Note that because we do not call this directly and instead the
         logging module does the calling and only knows about names, some
-        setup is deferred to the configure() method. If calling code
+        setup is deferred until the configure() method. If calling code
         uses the Logger factory function defined at the module level,
         this will be called automatically.
         """
@@ -82,16 +85,78 @@ class BufferedLogger(logging.Logger):  # many inherited, pylint: disable=R0904
         self._activated = False
         self._buffer = []
         self._stdout_flag = None
+        self._stdout_sh = logging.StreamHandler(stdout)
         self._file_flag = None
-        self._file_path = None
+        self._file_fh = None
 
-    def configure(self, stdout_flag, file_flag, file_path):
+    def configure(self, stdout_flag, file_flag, file_path, file_encoding):
         """
         Configures the flags that will trigger the logger to use stdout
         and file-based logging. Additionally, configures the logging
-        path to be used.
+        path and encoding to be used. If the logging path or encoding
+        has changed, the file will be closed, reopened, and reattached
+        to the logger.
         """
+
+        if (
+            not self._file_fh or
+            self._file_fh.baseFilename != file_path or
+            self._file_fh.encoding != file_encoding
+        ):
+            need_reattachment = False
+            if self._file_fh:
+                if self._file_fh in self.handlers:
+                    need_reattachment = True
+                self.removeHandler(self._file_fh)
+                self._file_fh.close()
+
+            self._file_fh = logging.FileHandler(
+                file_path,
+                encoding=file_encoding,
+                delay=True,
+            )
+
+            if need_reattachment:
+                self.addHandler(self._file_fh)
 
         self._stdout_flag = stdout_flag
         self._file_flag = file_flag
-        self._file_path = file_path
+
+    def _log(self, *args, **kwargs):
+        """
+        If we are activated, passes log messages to super class. If not,
+        buffers them to be passed after activation.
+        """
+
+        if self._activated:
+            if self.handlers:
+                super(BufferedLogger, self)._log(*args, **kwargs)
+
+        else:
+            self._buffer.append((args, kwargs))
+
+    def activate(self, lookup):
+        """
+        Attach or detach handlers based on the lookup dict passed and
+        the stdout_flag and file_flag that were passed to configure().
+        If this is the first time being activated, any buffered log
+        messages will be flushed out.
+        """
+
+        if self._stdout_flag and lookup[self._stdout_flag]:
+            self.addHandler(self._stdout_sh)
+        else:
+            self.removeHandler(self._stdout_sh)
+
+        if self._file_fh:
+            if self._file_flag and lookup[self._file_flag]:
+                self.addHandler(self._file_fh)
+            else:
+                self.removeHandler(self._file_fh)
+
+        if not self._activated:
+            self._activated = True
+
+            while len(self._buffer):
+                args, kwargs = self._buffer.pop(0)
+                self._log(*args, **kwargs)  # use magic, pylint: disable=W0142

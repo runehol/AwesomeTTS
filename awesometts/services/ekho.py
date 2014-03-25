11a8e30e4d508b577aeab655d774b3faf23cb1a1
Dave Shifflett <dave@corpulentcoffee.com>
2014-03-25 08:29:18 -0500

    Clean up whitespace in Ekho service
    Service code now conforms to a 4-space standard.

--- awesometts/services/ekho.py
+++ awesometts/services/ekho.py
@@ -40,71 +40,71 @@ from awesometts.paths import media_filename
 
 
 def playEkhoTTS(text, language):
-	text = re.sub("\[sound:.*?\]", "", stripHTML(text.replace("\n", "")).encode('utf-8'))
-	Popen(['ekho', '-v', language, text], stdin=PIPE, stdout=PIPE, stderr=STDOUT).communicate()
+    text = re.sub("\[sound:.*?\]", "", stripHTML(text.replace("\n", "")).encode('utf-8'))
+    Popen(['ekho', '-v', language, text], stdin=PIPE, stdout=PIPE, stderr=STDOUT).communicate()
 
 def playfromtagEkhoTTS(fromtag):
-	for item in fromtag:
-		match = re.match("(.*?):(.*)", item, re.M|re.I)
-		playEkhoTTS(match.group(2), match.group(1))
+    for item in fromtag:
+        match = re.match("(.*?):(.*)", item, re.M|re.I)
+        playEkhoTTS(match.group(2), match.group(1))
 
 def playfromHTMLtagEkhoTTS(fromtag):
-	for item in fromtag:
-		text = text = ''.join(item.findAll(text=True))
-		voice = item['voice']
-		playEkhoTTS(text, voice)
+    for item in fromtag:
+        text = text = ''.join(item.findAll(text=True))
+        voice = item['voice']
+        playEkhoTTS(text, voice)
 
 def get_language_id(language_code):
-	x = 0
-	for d in slanguages:
-		if d[0]==language_code:
-			return x
-		x = x + 1
+    x = 0
+    for d in slanguages:
+        if d[0]==language_code:
+            return x
+        x = x + 1
 
 
 def recordEkhoTTS(text, language):
-	text = re.sub("\[sound:.*?\]", "", stripHTML(text.replace("\n", "")).encode('utf-8'))
-	filename = media_filename(text, 'ekho', language, 'wav')
-	Popen(['ekho', '-v', language, '-t', 'wav', '-o', filename, text], stdin=PIPE, stdout=PIPE, stderr=STDOUT).communicate()
-	return filename
+    text = re.sub("\[sound:.*?\]", "", stripHTML(text.replace("\n", "")).encode('utf-8'))
+    filename = media_filename(text, 'ekho', language, 'wav')
+    Popen(['ekho', '-v', language, '-t', 'wav', '-o', filename, text], stdin=PIPE, stdout=PIPE, stderr=STDOUT).communicate()
+    return filename
 
 def filegenerator_layout(form):
-	global DefaultEkhoVoice
-	verticalLayout = QtGui.QVBoxLayout()
-	textEditlabel = QtGui.QLabel()
-	textEditlabel.setText("Language:")
-	form.comboBoxEkho = QtGui.QComboBox()
-	form.comboBoxEkho.addItems([d[1] for d in slanguages])
-	form.comboBoxEkho.setCurrentIndex(DefaultEkhoVoice) # get Default
+    global DefaultEkhoVoice
+    verticalLayout = QtGui.QVBoxLayout()
+    textEditlabel = QtGui.QLabel()
+    textEditlabel.setText("Language:")
+    form.comboBoxEkho = QtGui.QComboBox()
+    form.comboBoxEkho.addItems([d[1] for d in slanguages])
+    form.comboBoxEkho.setCurrentIndex(DefaultEkhoVoice) # get Default
 
-	verticalLayout.addWidget(textEditlabel)
-	verticalLayout.addWidget(form.comboBoxEkho)
-	return verticalLayout
+    verticalLayout.addWidget(textEditlabel)
+    verticalLayout.addWidget(form.comboBoxEkho)
+    return verticalLayout
 
 def recordEkhoTTS_form(form, text):
-	global DefaultEkhoVoice
-	DefaultEkhoVoice = form.comboBoxEkho.currentIndex() #set new Default
-	return recordEkhoTTS(text, slanguages[form.comboBoxEkho.currentIndex()][0])
+    global DefaultEkhoVoice
+    DefaultEkhoVoice = form.comboBoxEkho.currentIndex() #set new Default
+    return recordEkhoTTS(text, slanguages[form.comboBoxEkho.currentIndex()][0])
 
 def filegenerator_run(form):
-	global DefaultEkhoVoice
-	DefaultEkhoVoice = form.comboBoxEkho.currentIndex() #set new Default
-	return recordEkhoTTS(unicode(form.texttoTTS.toPlainText()), slanguages[form.comboBoxEkho.currentIndex()][0])
+    global DefaultEkhoVoice
+    DefaultEkhoVoice = form.comboBoxEkho.currentIndex() #set new Default
+    return recordEkhoTTS(unicode(form.texttoTTS.toPlainText()), slanguages[form.comboBoxEkho.currentIndex()][0])
 
 def filegenerator_preview(form):
-	return playEkhoTTS(unicode(form.texttoTTS.toPlainText()), slanguages[form.comboBoxEkho.currentIndex()][0])
+    return playEkhoTTS(unicode(form.texttoTTS.toPlainText()), slanguages[form.comboBoxEkho.currentIndex()][0])
 
 
 DefaultEkhoVoice = get_language_id('Mandarin')
 
 
 if not mswindows:
-	TTS_service = {'ekho' : {
-	'name': 'Ekho',
-	'play' : playEkhoTTS,
-	'playfromtag' : playfromtagEkhoTTS,
-	'playfromHTMLtag' : playfromHTMLtagEkhoTTS,
-	'record' : recordEkhoTTS_form,
-	'filegenerator_layout': filegenerator_layout,
-	'filegenerator_preview': filegenerator_preview,
-	'filegenerator_run': filegenerator_run}}
+    TTS_service = {'ekho' : {
+    'name': 'Ekho',
+    'play' : playEkhoTTS,
+    'playfromtag' : playfromtagEkhoTTS,
+    'playfromHTMLtag' : playfromHTMLtagEkhoTTS,
+    'record' : recordEkhoTTS_form,
+    'filegenerator_layout': filegenerator_layout,
+    'filegenerator_preview': filegenerator_preview,
+    'filegenerator_run': filegenerator_run}}

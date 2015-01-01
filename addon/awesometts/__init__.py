4d0d5a5fe0bc016f0f15d0a2d9198a9588c946d8
Dave Shifflett <dave@corpulentcoffee.com>
2015-01-01 11:35:00 -0600

    Bump copyright year
    Sets the main loader and package to show a copyright year of 2015 for
    the upcoming release.

--- addon/awesometts/__init__.py
+++ addon/awesometts/__init__.py
@@ -2,9 +2,9 @@
 
 # AwesomeTTS text-to-speech add-on for Anki
 #
-# Copyright (C) 2010-2014  Anki AwesomeTTS Development Team
+# Copyright (C) 2010-2015  Anki AwesomeTTS Development Team
 # Copyright (C) 2010-2013  Arthur Helfstein Fragoso
-# Copyright (C) 2013-2014  Dave Shifflett
+# Copyright (C) 2013-2015  Dave Shifflett
 # Copyright (C) 2012       Dominic Lerbs
 #
 # This program is free software: you can redistribute it and/or modify

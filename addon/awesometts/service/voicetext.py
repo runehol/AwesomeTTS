ed7e3f2d3f550c0cfdc9d291218dc18a0713590e
Dave Shifflett <dave@corpulentcoffee.com>
2016-05-29 18:55:07 -0500

    Bump copyright year in VoiceText module

--- addon/awesometts/service/voicetext.py
+++ addon/awesometts/service/voicetext.py
@@ -2,8 +2,8 @@
 
 # AwesomeTTS text-to-speech add-on for Anki
 #
-# Copyright (C) 2015       Anki AwesomeTTS Development Team
-# Copyright (C) 2015       Dave Shifflett
+# Copyright (C) 2015-2016  Anki AwesomeTTS Development Team
+# Copyright (C) 2015-2016  Dave Shifflett
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by

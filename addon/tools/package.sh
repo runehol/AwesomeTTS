d31ea72094dbe77f2d8c0cbf6bf1c9e850629763
Dave Shifflett <dave@corpulentcoffee.com>
2016-12-29 15:36:37 -0600

    Exit scripts immediately if any command fails

--- addon/tools/package.sh
+++ addon/tools/package.sh
@@ -16,6 +16,8 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+set -e
+
 if [ -z "$1" ]
 then
     echo 'Please specify where you want to save the package.' 1>&2
--- addon/tools/symlink.sh
+++ addon/tools/symlink.sh
@@ -16,6 +16,8 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+set -e
+
 if [ -z "$1" ]
 then
     echo 'Please specify your Anki addons directory.' 1>&2
